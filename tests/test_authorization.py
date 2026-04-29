9a95156fcd2e76bdce1ff14dfd116afeb8535feb
ponomarev.dv <graiwers@gmail.com>
2026-04-29 17:00:45 +0400

    test autoriztion and cart with allure

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,41 +1,66 @@
 from playwright.sync_api import expect
+import allure
 
+@allure.title("Тест авторизации")
 def test_login(login_page):
-    page = login_page.login()
-    expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
+    with allure.step('Авторизация'):
+        page = login_page.login()
+    with allure.step('Проверка перехода на страницу магазина'):
+        expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
 
+@allure.title("Тест выход из аккаунта")
 def test_logout(login_page):
-    page = login_page.login()
-    expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
+    with allure.step('Авторизация'):
+        page = login_page.login()
+    with allure.step('Проверка перехода на страницу магазина'):
+        expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
 
-    page.locator('#react-burger-menu-btn').click()
-    page.locator("#logout_sidebar_link").click()
-    expect(page).to_have_url('https://www.saucedemo.com/')
+    with allure.step('Нажатие лог-оут'):
+        page.locator('#react-burger-menu-btn').click()
+        page.locator("#logout_sidebar_link").click()
+    with allure.step('Проверка перехода на страницу авторизации'):
+        expect(page).to_have_url('https://www.saucedemo.com/')
 
+@allure.title("Тест авторизации с неверным именем")
 def test_login_bad_username(login_page):
-    page = login_page.login(username="invalid_user")
+    with allure.step('Авторизация с неверным логином'):
+        page = login_page.login(username="invalid_user")
 
-    expect(page).to_have_url('https://www.saucedemo.com/')
-    (expect(page.locator('[data-test=\"error\"]'))
-     .to_contain_text("Username and password do not match any user in this service"))
+    with allure.step('Остается на странице авторизации'):
+        expect(page).to_have_url('https://www.saucedemo.com/')
+    with allure.step('Проверка появления сообщения о неверном имени'):
+        (expect(page.locator('[data-test=\"error\"]'))
+         .to_contain_text("Username and password do not match any user in this service"))
 
+@allure.title("Тест авторизации с пустым именем")
 def test_login_empty_username(login_page):
-    page = login_page.login(username="")
+    with allure.step('Авторизация с пустым логином'):
+        page = login_page.login(username="")
 
-    expect(page).to_have_url('https://www.saucedemo.com/')
-    (expect(page.locator('[data-test=\"error\"]'))
-     .to_contain_text("Username is required"))
+    with allure.step('Остается на странице авторизации'):
+        expect(page).to_have_url('https://www.saucedemo.com/')
+    with allure.step('Проверка появления сообщения о пустом имени'):
+        (expect(page.locator('[data-test=\"error\"]'))
+         .to_contain_text("Username is required"))
 
+@allure.title("Тест авторизации с пустым паролем")
 def test_login_empty_password(login_page):
-    page = login_page.login(password="")
+    with allure.step('Авторизация с пустым паролем'):
+        page = login_page.login(password="")
 
-    expect(page).to_have_url('https://www.saucedemo.com/')
-    (expect(page.locator('[data-test=\"error\"]'))
-     .to_contain_text("Password is required"))
+    with allure.step('Остается на странице авторизации'):
+        expect(page).to_have_url('https://www.saucedemo.com/')
+    with allure.step('Проверка появления сообщения о пустом пароле'):
+        (expect(page.locator('[data-test=\"error\"]'))
+        .to_contain_text("Password is required"))
 
+@allure.title("Тест авторизации с заблокированным аккаунтом")
 def test_login_locked_user(login_page):
-    page = login_page.login(username='locked_out_user')
+    with allure.step('Авторизация с заблокированным аккаунтом'):
+        page = login_page.login(username='locked_out_user')
 
-    expect(page).to_have_url('https://www.saucedemo.com/')
-    (expect(page.locator('[data-test=\"error\"]'))
-     .to_contain_text("Sorry, this user has been locked out"))
+    with allure.step('Остается на странице авторизации'):
+        expect(page).to_have_url('https://www.saucedemo.com/')
+    with allure.step('Проверка появления сообщения о заблокированном аккаунте'):
+        (expect(page.locator('[data-test=\"error\"]'))
+         .to_contain_text("Sorry, this user has been locked out"))
--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,64 +1,92 @@
 from playwright.sync_api import expect
+import allure
 
+@allure.title("Тест добавления и удаление товара из корзины")
 def test_button_add_to_cart_and_remove(login_page):
-    page = login_page.login()
+    with allure.step('Авторизация'):
+        page = login_page.login()
 
-    page.locator('.btn_primary').first.click()
-    expect(page.locator('.shopping_cart_badge')).to_contain_text('1')
+    with allure.step('Добавление товара в корзину'):
+        page.locator('.btn_primary').first.click()
+    with allure.step('Увеличения товара в корзине'):
+        expect(page.locator('.shopping_cart_badge')).to_contain_text('1')
 
-    page.locator('.btn_secondary ').first.click()
-    expect(page.locator('.shopping_cart_badge')).to_have_count(0)
+    with allure.step('Удаление товара из корзины'):
+        page.locator('.btn_secondary ').first.click()
+    with allure.step('Уменьшения товара в корзине'):
+        expect(page.locator('.shopping_cart_badge')).to_have_count(0)
 
+@allure.title("Проверка заполнения карточки товара в корзине")
 def test_check_item_in_cart(login_page):
-    page = login_page.login()
+    with allure.step('Авторизация'):
+        page = login_page.login()
 
-    page.locator('.btn_primary').first.click()
-    expect(page.locator('.shopping_cart_badge')).to_contain_text('1')
+    with allure.step('Добавление товара в корзину'):
+        page.locator('.btn_primary').first.click()
+    with allure.step('Увеличения товара в корзине'):
+        expect(page.locator('.shopping_cart_badge')).to_contain_text('1')
 
-    page.locator('.shopping_cart_link').click()
-    expect(page).to_have_url('https://www.saucedemo.com/cart.html')
+    with allure.step('Переход в корзину'):
+        page.locator('.shopping_cart_link').click()
+        expect(page).to_have_url('https://www.saucedemo.com/cart.html')
 
-    expect(page.locator('.cart_item')).not_to_be_empty()
-    expect(page.locator('.inventory_item_name')).to_be_visible()
-    expect(page.locator('.inventory_item_desc')).to_be_visible()
-    expect(page.locator('.inventory_item_price')).to_be_visible()
+    with allure.step('Корзина не пустая'):
+        expect(page.locator('.cart_item')).not_to_be_empty()
+    with allure.step('В карточке товара есть наименование, описание, цена'):
+        expect(page.locator('.inventory_item_name')).to_be_visible()
+        expect(page.locator('.inventory_item_desc')).to_be_visible()
+        expect(page.locator('.inventory_item_price')).to_be_visible()
 
+@allure.title("Тест удаления товара из внутри корзины")
 def test_delete_item_in_cart(login_page):
-    page = login_page.login()
+    with allure.step('Авторизация'):
+        page = login_page.login()
 
-    page.locator('.btn_primary').first.click()
-    expect(page.locator('.shopping_cart_badge')).to_contain_text('1')
+    with allure.step('Добавление товара в корзину'):
+        page.locator('.btn_primary').first.click()
+        expect(page.locator('.shopping_cart_badge')).to_contain_text('1')
 
-    page.locator('.shopping_cart_link').click()
-    expect(page).to_have_url('https://www.saucedemo.com/cart.html')
+    with allure.step('Переход в корзину'):
+        page.locator('.shopping_cart_link').click()
+        expect(page).to_have_url('https://www.saucedemo.com/cart.html')
 
-    page.locator('.btn_secondary').first.click()
-    expect(page.locator('.cart_item')).to_have_count(0)
+    with allure.step('Удаление товара из корзины'):
+        page.locator('.btn_secondary').first.click()
+        expect(page.locator('.cart_item')).to_have_count(0)
 
-
+@allure.title("Тест оформления заказа")
 def test_purchase_item(login_page):
-    page = login_page.login()
+    with allure.step('Авторизация'):
+        page = login_page.login()
 
-    page.locator('.btn_primary').first.click()
-    expect(page.locator('.shopping_cart_badge')).to_contain_text('1')
-    page.locator('.shopping_cart_link').click()
-    expect(page).to_have_url('https://www.saucedemo.com/cart.html')
+    with allure.step('Добавление товара в корзину'):
+        page.locator('.btn_primary').first.click()
+        expect(page.locator('.shopping_cart_badge')).to_contain_text('1')
 
-    page.locator('#checkout').click()
-    expect(page).to_have_url('https://www.saucedemo.com/checkout-step-one.html')
-    page.locator('#first-name').fill('Oleg')
-    page.locator('#last-name').fill('Popov')
-    page.locator('#postal-code').fill('123-456')
-    page.locator('#continue').click()
-    expect(page).to_have_url('https://www.saucedemo.com/checkout-step-two.html')
-    expect(page.locator('.cart_item')).not_to_be_empty()
-    expect(page.locator('.inventory_item_name')).to_be_visible()
-    expect(page.locator('.inventory_item_desc')).to_be_visible()
-    expect(page.locator('.inventory_item_price')).to_be_visible()
+    with allure.step('Переход в корзину'):
+        page.locator('.shopping_cart_link').click()
+        expect(page).to_have_url('https://www.saucedemo.com/cart.html')
 
-    page.locator('#finish').click()
-    expect(page).to_have_url('https://www.saucedemo.com/checkout-complete.html')
+    with allure.step('Первый этап оформления'):
+        page.locator('#checkout').click()
+        expect(page).to_have_url('https://www.saucedemo.com/checkout-step-one.html')
+        page.locator('#first-name').fill('Oleg')
+        page.locator('#last-name').fill('Popov')
+        page.locator('#postal-code').fill('123-456')
 
-    expect(page.locator('.complete-header')).to_contain_text('Thank you for your order!')
-    page.locator('#back-to-products').click()
-    expect(page).to_have_url('https://www.saucedemo.com/inventory.html')
+    with allure.step('Второй этап оформления'):
+        page.locator('#continue').click()
+        expect(page).to_have_url('https://www.saucedemo.com/checkout-step-two.html')
+        expect(page.locator('.cart_item')).not_to_be_empty()
+        expect(page.locator('.inventory_item_name')).to_be_visible()
+        expect(page.locator('.inventory_item_desc')).to_be_visible()
+        expect(page.locator('.inventory_item_price')).to_be_visible()
+
+    with allure.step('Подтверждение заказа'):
+        page.locator('#finish').click()
+        expect(page).to_have_url('https://www.saucedemo.com/checkout-complete.html')
+        expect(page.locator('.complete-header')).to_contain_text('Thank you for your order!')
+
+    with allure.step('Переход в магазин по завершению покупки'):
+        page.locator('#back-to-products').click()
+        expect(page).to_have_url('https://www.saucedemo.com/inventory.html')
